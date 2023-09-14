f2672253002eecaaa3c7ed99bb3b9579a88aa1fd
victor Chassaigne <vchassaigne@proton.me>
2023-09-14 16:04:43 -0400

    added Create_slice function

--- utisli/slice_utility.go
+++ utisli/slice_utility.go
@@ -1,6 +1,9 @@
 package utisli
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 // return an float64 , which is an averahge of []int , return 1 after coma : 10,3
 func Average_slice_int(the_slice []int) float64 {
@@ -16,7 +19,7 @@ func Average_slice_int(the_slice []int) float64 {
 	return final
 }
 
-//return the number of times a string is present in a slice of strings
+// return the number of times a string is present in a slice of strings
 func Number_string_slice(the_slice []string, word string) int {
 	var num int
 	for _, v := range the_slice {
@@ -26,5 +29,29 @@ func Number_string_slice(the_slice []string, word string) int {
 
 	}
 	return num
+
+}
+
+// create a slice of int ,min and max value you can use
+func Create_slice_int(min, max int) []int {
+	var out_slice []int
+	//loop to create slice
+	for {
+		var to_add int
+		fmt.Println("type int to add,-1 to stop ")
+		fmt.Scan(&to_add)
+		if to_add == -1 {
+			fmt.Println("returning", out_slice)
+			return out_slice
+		}
+		if to_add < min || to_add > max {
+			fmt.Println("error with range , try again")
+			continue
+		} else {
+			out_slice = append(out_slice, to_add)
+
+		}
+
+	}
 
 }
